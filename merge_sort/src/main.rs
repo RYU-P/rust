074050863807f676f080ceac2d027c4072eaef1c
ryu <ryujipy@gmail.com>
2023-07-21 14:14:29 -0400

    made vector and slice functions

--- merge_sort/src/main.rs
+++ merge_sort/src/main.rs
@@ -1,3 +1,31 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let v = random_vector(10, 2);
+    println!("{:?}", v);
+
+    
+}
+
+fn random_vector(size: usize, max: i32) -> Vec<i32> {
+    let mut rng = rand::thread_rng();
+    let mut v = Vec::with_capacity(size);
+    for _ in 0..size {
+        v.push(rng.gen_range(0..max));
+    }
+    v
+}
+
+fn sum_slice(s: &[i32]) -> i32 {
+    s.iter().sum()
+}
+
+fn linear_search(s: &[i32], target: i32) -> usize {
+    s.iter().position(|&num| {
+        num == target
+    }).expect("no element that exists that equal target")
+} 
+
+fn selection_sort(v: &mut Vec<i32>, size: usize) {
+    
 }
