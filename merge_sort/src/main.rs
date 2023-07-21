e63449c4f84c8e3815f90f85719675684d3c140a
Dillon-Roller <dillonroller.18@gmail.com>
2023-07-21 18:59:00 -0400

    started merge sort

--- merge_sort/src/main.rs
+++ merge_sort/src/main.rs
@@ -25,4 +25,36 @@ fn linear_search(s: &[i32], target: i32) -> usize {
         .expect("no element that exists that equal target")
 }
 
-fn selection_sort(v: &mut Vec<i32>, size: usize) {}
+fn selection_sort(v: &mut Vec<i32>, size: usize) {
+
+}
+
+fn merge_sort(arr: &mut [i32]) {
+    let len = arr.len();
+    if len < 2 {
+        return
+    }
+    let mid = len / 2;
+
+    merge_sort(&mut arr[..mid]);
+    merge_sort(&mut arr[mid..]);
+
+    let aux = arr.to_vec();
+
+    merge(, right, arr);
+}
+
+fn merge(left: &[i32], right: &[i32], arr: &mut [i32]) {
+    let (mut i, mut j, mut k) = (0, 0, 0);
+
+    while i < left.len() && j < right.len() {
+        if left[i] <= right[j] {
+            arr[k] = left[i];
+            i += 1;
+        } else {
+            arr[k] = right[j];
+            j += 1;
+        }
+        k += 1;
+    }
+}
