eeea4633619e1e7c26aca851df4aff0304569056
ryu <ryujipy@gmail.com>
2023-08-29 13:46:12 -0400

    going to do linked list

---
+++ leetcode/group_anagram/src/main.rs
@@ -0,0 +1,11 @@
+struct Solution;
+
+impl Solution {
+    pub fn group_anagram(strs: Vec<String>) -> Vec<Vec<String>> {
+       //how would i solve this  
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
