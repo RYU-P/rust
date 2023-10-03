9c70622a11b107126732c422a73cbb239e8fa946
ryu <ryujipy@gmail.com>
2023-10-03 02:55:19 -0400

    dioxus tutorial web + fullstack

--- leetcode/reorder_list/src/main.rs
+++ leetcode/reorder_list/src/main.rs
@@ -21,10 +21,22 @@ impl ListNode {
   }
 }
 
-struct Solution:
+struct Solution;
 
 impl Solution {
     pub fn reorder_list(head: &mut Option<Box<ListNode>>) {
+      //split the list in half and go from index 0, n, 1, n-1, until it reaches the middle element. it works because last element will always be the middle(index) element.
+      //example: length/2=middle, 
+      //4/2=2, 1 -> 2 -> 3 -> 4 and 1 -> 4 -> 2 -> 3.
+      //5/2=2  1 -> 2 -> 3 -> 4 -> 5 and 1 -> 5 -> 2 -> 4 -> 3, 
+      //slow and fast pointer technique:
+      //slow pointer moves one node at a time, fast pointer moves two nodes at a time. when fast pointer reaches the end, slow pointer will be at the middle.    
+      let mut slow = &mut head.as_mut();
+      let mut fast = &mut head.as_mut();
+      while fast.is_some() && fast.is_some() {
+        let slow = &mut unwrap().next.as_mut();
+        let fast = &mut fast.unwrap().next.as_mut().unwrap().next;
+      }
 
     }
 }
